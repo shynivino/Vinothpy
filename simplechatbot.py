e1cb354766af37bf22f65b306014e2af3ded6452
Doddahulugappa B <doddahulugappa@gmail.com>
2017-05-01 10:54:41 +0530

    Create simplechatbot.py

---
+++ simplechatbot.py
@@ -0,0 +1,17 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+chatbot = ChatBot("Ron Obvious")
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+chatbot.set_trainer(ListTrainer)
+chatbot.train(conversation)
+response = chatbot.get_response("How are you!")
+print(response)
