58826924b7a72e69f0bbae094b97d8fb88aba568
Doddahulugappa B <doddahulugappa@gmail.com>
2017-05-03 09:16:20 +0530

    Update commandlineArgs.py

--- commandlineArgs.py
+++ commandlineArgs.py
@@ -2,3 +2,8 @@ import sys
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', sys.argv)
+
+##OUTPUT##
+##>python commandlineArgs.py.py huli puli
+##Number of arguments: 3 arguments.
+##Argument List: ['C:/Users/b.doddahulugappa/AppData/Local/Programs/Python/Python35/trial.py', 'huli', 'puli']
