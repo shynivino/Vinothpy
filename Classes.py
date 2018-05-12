f10a589fe61bcf35a7a1826dd484409e11d54814
Doddahulugappa B <doddahulugappa@users.noreply.github.com>
2018-05-12 16:09:30 +0530

    Update Classes.py
    Operator overloading

--- Classes.py
+++ Classes.py
@@ -1,3 +1,5 @@
+
+## Staic, Insatnce and Class methods
 class Student:  
    empcount=0  
    def __init__(self, rollno, name):  
@@ -17,6 +19,16 @@ class Student:
        else:
            return("not eligible")
 
+emp1 = Student(121, "Ajeet")  
+emp2 = Student(122, "Sonoo")  
+
+emp1.displayStudent()  
+emp2.displayStudent()
+Student.printemployeecount()
+print(Student.checkeligibility(18))
+
+         
+## Polymorphism
 class Parrot:
 
     def fly(self):
@@ -43,11 +55,31 @@ peggy = Penguin()
 # passing the object
 flying_test(blu) #calls Parrot fly method
 flying_test(peggy) #calls Penguin fly method
+      
+## Operator Overloading     
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
 
-emp1 = Student(121, "Ajeet")  
-emp2 = Student(122, "Sonoo")  
+    #def __str__(self):
+        #return "({0},{1})".format(self.x,self.y)
+    def __repr__(self):
+        return "({0},{1})".format(self.x,self.y)
 
-emp1.displayStudent()  
-emp2.displayStudent()
-Student.printemployeecount()
-print(Student.checkeligibility(18))
+    def __add__(self,other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x,y)
+
+
+p1= Point(2,3)
+p2 = Point(-1,2)
+print(p1)
+print(p2)
+print(p1+p2)
+
+
+
+
+
