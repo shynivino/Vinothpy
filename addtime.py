d522d467e7bc7812b98da90452f0debd24af2d1f
doddahulugappa <doddahulugappagmail.com>
2018-10-19 12:37:53 +0400

    adding two timestamps

---
+++ addtime.py
@@ -0,0 +1,10 @@
+import datetime
+import time
+i = 5
+time1 = datetime.datetime.strptime("2018-10-18 18:50:57.606989",'%Y-%m-%d %H:%M:%S.%f')
+print(time1)
+while( i <= 100):
+    print(time1  +  datetime.timedelta(minutes = i))
+    time.sleep(2)
+    i = i + 5
+
