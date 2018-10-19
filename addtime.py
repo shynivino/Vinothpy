0e9d12fb16358de3518bb7fd7729249684b3640f
doddahulugappa <doddahulugappagmail.com>
2018-10-19 15:48:58 +0400

    added comment

--- addtime.py
+++ addtime.py
@@ -4,7 +4,7 @@ i = 5
 time1 = datetime.datetime.strptime("2018-10-18 18:50:57.606989",'%Y-%m-%d %H:%M:%S.%f')
 print(time1)
 while( i <= 100):
-    print(time1  +  datetime.timedelta(minutes = i))
+    print(time1  +  datetime.timedelta(minutes = i)) # adds 5 minutes to the time1
     time.sleep(2)
     i = i + 5
 
