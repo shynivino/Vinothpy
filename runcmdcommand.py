59789f6096ceee9403733fc13614ba47cd8a9aeb
doddahulugappa <doddahulugappagmail.com>
2018-11-23 08:48:13 +0400

    json parser
    Signed-off-by: doddahulugappa <doddahulugappagmail.com>

--- runcmdcommand.py
+++ runcmdcommand.py
@@ -1,5 +1,13 @@
-import os
-os.system(r'tasklist/svc > tmp.txt')
+import os,re
+
+os.system(r'net user administrator > tmp.txt')
 lines = open('tmp.txt', 'r').read()
 os.remove('tmp.txt')
 print(lines)
+lines = lines.split('\n')
+for line in lines:
+    if re.search("Local Group Memberships.*\*Admin.*",line):
+        print("ADMIN:",line);
+    if re.search("Password last set.*",line):
+        print("pwd:",line)
+        
